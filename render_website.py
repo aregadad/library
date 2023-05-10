094293c904ba5a94830a434b1b2359098c175376
aregadad <biafleks@gmail.com>
2023-05-10 20:14:59 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -5,6 +5,7 @@ import json
 from os import sep
 from more_itertools import chunked
 from pathlib import Path
+from math import ceil
 
 
 env = Environment(
@@ -19,13 +20,18 @@ with open('books.json', encoding='utf8') as file:
 
 pages_path = Path('pages')
 pages_path.mkdir(parents=True, exist_ok=True)
-for index, books in enumerate(chunked(books, 20), 1):
+books_per_page = 20
+columns_per_page = 2
+max_page_num=ceil(len(books) / books_per_page)
+for page_num, books in enumerate(chunked(books, books_per_page), 1):
     rendered_page = template.render(
-        chunked_books=chunked(books, 2),
-        sep=sep
+        chunked_books=chunked(books, columns_per_page),
+        sep=sep,
+        current_page_num=page_num,
+        max_page_num=max_page_num,
     )
 
-    with open(pages_path / f'index{index}.html', 'w', encoding="utf8") as file:
+    with open(pages_path / f'index{page_num}.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 def on_reload():
